99264c642e2b8204f09fe40ab8020bdf77cb27a6
Your Name <you@example.com>
2022-03-26 13:58:31 +0300

    смена имени класса

--- test_items.py
+++ test_items.py
@@ -4,7 +4,7 @@ import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-class TestCatalogwebsite():
+class TestCatalogePage():
  def test_guest_should_see_login_link(self, browser):
      browser.get(link)
      time.sleep(10)
