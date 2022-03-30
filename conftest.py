40739958070190cd3e03a6030ed377e256e8df08
Your Name <you@example.com>
2022-03-30 09:47:55 +0300

    correct space

--- conftest.py
+++ conftest.py
@@ -6,17 +6,17 @@ def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default='firefox',
                      help="Choose browser: chrome or firefox")
     parser.addoption('--language', action='store', default='ru',
-                     help="Choose language: ru, en-gb, es, fr")
+                     help="Choose language")
 
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
     user_language = request.config.getoption("language")
     if browser_name == "chrome":
-      options = Options()
-      options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-      print("\nstart chrome browser for test...")
-      browser = webdriver.Chrome(options=options)
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        print("\nstart chrome browser for test...")
+        browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
         fp = webdriver.FirefoxProfile()
         fp.set_preference("intl.accept_languages", user_language)
--- test_items.py
+++ test_items.py
@@ -3,13 +3,10 @@ import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-
 class TestCatalogePage():
- def test_item_has_add_to_cart_button(self, browser):
-     browser.get(link)
-     time.sleep(10)
-     button = browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
-     assert button,"корзина не найдена"
-
-
+    def test_item_has_add_to_cart_button(self, browser):
+        browser.get(link)
+        time.sleep(10)
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
+        assert button,"корзина не найдена"
 
